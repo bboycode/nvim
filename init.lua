809b0b3bd3d850353d908dba690c6727dc7aa956
Britton_laptop <25952277@sun.ac.za>
2023-06-28 12:18:29 +0200

    added toykonight colorsheme

--- init.lua
+++ init.lua
@@ -131,10 +131,10 @@ require('lazy').setup({
 
   {
     -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
+    "folke/tokyonight.nvim",
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
+      vim.cmd.colorscheme 'tokyonight'
     end,
   },
 
@@ -145,7 +145,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
+        theme = 'tokyonight',
         component_separators = '|',
         section_separators = '',
       },
