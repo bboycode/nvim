d52f7e5cead530bac8de9ab81a6a2191b204f176
Britton_laptop <25952277@sun.ac.za>
2023-10-12 19:33:54 +0200

    forgot to update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -71,7 +71,7 @@ return{
     "folke/tokyonight.nvim",
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'tokyonight'
+      vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
 
